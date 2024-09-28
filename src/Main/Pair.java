78b58adee6f71b07ef6b5a22b7dcca5c788496a3
Ahmed-Elgamel <ahmed_hamed2242@yahoo.com>
2024-09-28 16:00:30 +0300

    whole project commit

---
+++ src/Main/Pair.java
@@ -0,0 +1,12 @@
+package Main;
+
+
+public class Pair{
+    Object first;
+    Object second;
+    public 	Pair(Object first, Object second) {
+        this.first = first;
+        this.second = second;
+    }
+}
+
